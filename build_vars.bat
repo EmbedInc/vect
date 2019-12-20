62e49bff097be5d9c086b1aa3cc8a97233dc8370
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-12-20 14:15:24 -0500

    Added support for build debug settings.

--- build_vars.bat
+++ build_vars.bat
@@ -7,6 +7,6 @@ set buildname=
 call treename_var "(cog)source/vect" sourcedir
 set libname=vect
 set fwname=
-set pictype=
-set picclass=
-set t_parms=
+call treename_var "(cog)src/%srcdir%/debug_%fwname%.bat" tnam
+make_debug "%tnam%"
+call "%tnam%"
